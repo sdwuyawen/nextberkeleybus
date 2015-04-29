0cb9ac2f5f6ffe22cd718e1a35794ea33c2284e8
Angus H. <10vx6o+5wujme7vftqmo@sharklasers.com>
2015-04-28 21:30:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,9 @@ var perimeter =
 
 };
 
+// Copyright (c) 2015 Angus H. (4148)
+// Distributed under the GNU General Public License v3.0 (GPLv3).
+
 // array of bus times
 var buses = {'Central Campus': central, 'Perimeter Line': perimeter};
 
